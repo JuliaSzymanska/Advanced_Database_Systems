a6cbf9b0a8ba4fa4c7a11bfb6cfff1c62f7ad711
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-07 18:04:29 +0200

    To 8 ex

--- ZSBD_Zadanie_1/SQLQuery.sql
+++ ZSBD_Zadanie_1/SQLQuery.sql
@@ -14,13 +14,36 @@ WHERE id_kraju NOT IN (SELECT id_kraju FROM zawodnicy);
 GO
 
 --3--
-SELECT k.kraj, COUNT(id_skoczka) FROM kraje k, zawodnicy z
-WHERE k.id_kraju = z.id_kraju
-GROUP BY kraj;
-GO
+
 
 --4--
 SELECT nazwisko FROM zawodnicy
 WHERE id_skoczka NOT IN (SELECT id_skoczka FROM uczestnictwa_w_zawodach);
 GO
 
+--5--
+SELECT z.nazwisko, COUNT(z.id_skoczka) AS ile FROM zawodnicy z, uczestnictwa_w_zawodach u
+WHERE z.id_skoczka = u.id_skoczka
+GROUP BY nazwisko;
+GO
+
+--6--
+SELECT z.nazwisko, s.nazwa FROM zawodnicy z, uczestnictwa_w_zawodach u, zawody, skocznie s
+WHERE z.id_skoczka = u.id_skoczka
+AND u.id_zawodow = zawody.id_zawodow
+AND zawody.id_skoczni = s.id_skoczni
+GROUP BY nazwisko, nazwa;
+GO
+
+--7--
+SELECT nazwisko, DATEDIFF(year,  data_ur, GETDATE()) AS wiek FROM zawodnicy
+ORDER BY wiek DESC;
+
+--8--
+SELECT z.nazwisko, DATEDIFF(year, z.data_ur, zawody.DATA) FROM zawody, zawodnicy z,  uczestnictwa_w_zawodach u
+WHERE z.id_skoczka = u.id_skoczka
+AND u.id_zawodow = zawody.id_zawodow
+AND zawody.DATA IN (SELECT DATA FROM (SELECT z.id_skoczka, MIN(zawod.DATA) as DATA FROM zawody zawod, uczestnictwa_w_zawodach uc, zawodnicy z
+							WHERE z.id_skoczka = uc.id_skoczka
+							AND uc.id_zawodow = zawod.id_zawodow GROUP BY zawod.DATA, z.id_skoczka) AS tab WHERE tab.id_skoczka = z.id_skoczka)
+ORDER BY z.nazwisko
